14f5285e0b66914cd8ed55233dd188dac11770b3
Sébastien Gava <sebastiengava@MacBook-Pro-de-Sebastien.local>
2023-10-05 01:41:44 +0200

    (WIP) added glog, not working yet

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <glog/logging.h>
 
-int main()
+int main(int argc, char const *argv[])
 {
-    std::cout << "Hello" << std::endl;
+    // Initialize Google’s logging library.
+    google::InitGoogleLogging(argv[0]);
+
+    FLAGS_minloglevel = google::INFO;  // Set minimum log level to INFO
+    FLAGS_logtostderr = false;  // Disable logging to stderr
+    FLAGS_alsologtostderr = false;  // Disable duplication of log messages to stderr
+    FLAGS_log_dir = "./logs";  // Set log directory
+    google::SetLogDestination(google::GLOG_INFO, "./logs/program.log");
+    LOG(INFO) << "Found " << " cookies";
+    LOG(WARNING) << "Something Bad";
+    LOG(ERROR) << "Something Really Bad";
     return 0;
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "gtest/gtest.h"
+#include "MyClass.hpp"
 
 TEST(example, subtract) {
-    ASSERT_EQ(0,0);
+    MyClass mc;
+    ASSERT_EQ(10,mc.foo(5));
 }
