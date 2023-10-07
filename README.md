ff55f8465b9bc42bae64cc132eefa3f64ef9803c
Sébastien Gava <gava.seb@gmail.com>
2023-10-07 23:11:54 +0200

    WIP - Improvements

--- README.md
+++ README.md
@@ -1,19 +1,48 @@
+# Template C++/CMake Project
+A Template project for building C++ applications with cmake. Unit tests with GoogleTest.
+## Requirements
+* CMake (version ?)
+* Git
+* Doxygen (optional)
+
+## Dependencies
+* *GoogleTest* for unit testing
+* *GLog* for logging
+
+# How to use
+
+## Setup
 ```bash
 git clone ...
 git submodule update --init --recursive
 ```
-
-
+## Build
 ```bash
-cmake -S . -B build/MyProject
-cmake --build build/MyProject
-./build/MyProject/MyProject
+cmake -S . -B build
+cmake --build build
 ```
 
+## Run
+```bash
+./build/apps/app
+```
+By default will create a *logs* directory in the current directory, to specify another directory use:
+```bash
+./build/apps/app --log_dir=/some/directory
+```
+For other options, check glog documentation.
+
+## Tests
 ```bash
 cmake -S tests -B build/tests
 cmake --build build/tests
-./build/test/MyTests
+./build/tests/MyProject_test
 ```
 
 To collect code coverage information, run CMake with the `-DENABLE_TEST_COVERAGE=1` option.
+
+## Documentation (requires Doxygen)
+```bash
+cmake --build build --target docs
+```
+Result in *build/docs/html*
--- apps/main.cpp
+++ apps/main.cpp
@@ -1,22 +1,29 @@
-#include <iostream>
+#include <string>
+#include <filesystem>
 #include <glog/logging.h>
 
+// Project specific
 #include "MyClass.hpp"
 
-int main(int argc, char const *argv[])
+// Default path of the log directory if not specified with --log_dir
+const std::string DEFAULT_LOG_PATH{ "./logs" };
+
+int main(int argc, char *argv[])
 {
-    // Initialize Google’s logging library.
+    /* Initialize Google’s logging library. */
+    google::ParseCommandLineFlags(&argc, &argv, true);
+    // If not specified, use default log path
+    if(FLAGS_log_dir == "") {
+        FLAGS_log_dir = DEFAULT_LOG_PATH;
+    }
+    // Check if the log directory exists, and create it if it doesnt
+    if (!std::filesystem::exists(FLAGS_log_dir)) {
+        std::filesystem::create_directories(FLAGS_log_dir);
+    }
     google::InitGoogleLogging(argv[0]);
-
-    FLAGS_minloglevel = google::INFO;  // Set minimum log level to INFO
-    FLAGS_logtostderr = false;  // Disable logging to stderr
-    FLAGS_alsologtostderr = false;  // Disable duplication of log messages to stderr
-    FLAGS_log_dir = "./logs";  // Set log directory
-    google::SetLogDestination(google::GLOG_INFO, "./logs/program.log");
-    LOG(INFO) << "Found " << " cookies";
-    LOG(WARNING) << "Something Bad";
-    LOG(ERROR) << "Something Really Bad";
-
+    
+    /* Start the application */
+    LOG(INFO) << "--- Starting Application ---";
     MyClass mc;
     
     return 0;
